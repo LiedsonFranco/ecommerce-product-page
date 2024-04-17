15f0542cc265d935c99167a260bdba64628cefa8
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-04-17 12:02:03 +0100

    mobile design completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['/*/**.{html,js}'],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        skin:{
+          'Orange': 'hsl(26, 100%, 55%)',
+          'Pale-orange': 'hsl(25, 100%, 94%)',
+          'Dark-grayish-blue': 'hsl(219, 9%, 45%)',
+          'Grayish-blue': 'hsl(220, 14%, 75%)',
+        }
+      },
+      backgroundColor:{
+        skin:{
+          'Pale-orange': 'hsl(25, 100%, 94%)',
+          'Grayish-blue': 'hsl(220, 14%, 75%)',
+          'Light-grayish-blue': 'hsl(223, 64%, 98%)',
+          'Orange': 'hsl(26, 100%, 55%)',
+        }
+      }
+    },
   },
   plugins: [],
 }
